c695c0f5e5bf71ba4ba18a3184777c774cd5c9f1
Ridwan Syabani <2103010116@unper.ac.id>
2024-05-28 22:39:42 +0700

    menambahkan login page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,3 @@
-<?php
+use App\Http\Controllers\LoginController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
